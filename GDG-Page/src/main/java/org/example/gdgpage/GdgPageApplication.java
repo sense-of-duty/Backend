e2e7beb7c30281e48589a36eb7c7667ec61c776b
0xwb7 <woobin200111@gmail.com>
2025-11-24 22:17:30 +0900

    chore: init spring boot project environment - Spring Boot 3.x. Java 17 기반 프로젝트 생성 - gradle 설정 추가 (lombok, spring web, devtools, mysql, security, jpa) - application.yml 설정 - 개발에 필요한 추가 설정이 있으면 본인 판단 하에 추가

---
+++ GDG-Page/src/main/java/org/example/gdgpage/GdgPageApplication.java
@@ -0,0 +1,13 @@
+package org.example.gdgpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdgPageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GdgPageApplication.class, args);
+    }
+
+}
---
+++ GDG-Page/src/test/java/org/example/gdgpage/GdgPageApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gdgpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GdgPageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

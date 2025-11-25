039b13150fed2ec9d1e829dda5a63438a1735160
kimeoyeon <djdus126@gmail.com>
2025-11-26 01:58:18 +0900

    feat: FreeComment 리포지토리 생성 (#2)

---
+++ GDG-Page/src/main/java/org/example/gdgpage/repository/freeboard/FreeCommentRepository.java
@@ -0,0 +1,8 @@
+package org.example.gdgpage.repository.freeboard;
+
+import org.example.gdgpage.domain.freeboard.FreeComment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FreeCommentRepository extends JpaRepository<FreeComment, Long> {
+
+}

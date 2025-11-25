f7aba0a43bfc22da4530b8653c86d5c7e6ff15b6
0xwb7 <woobin200111@gmail.com>
2025-11-25 11:17:21 +0900

    feat: create jwtfilter (#1)

---
+++ GDG-Page/src/main/java/org/example/gdgpage/jwt/JwtFilter.java
@@ -0,0 +1,34 @@
+package org.example.gdgpage.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.example.gdgpage.exception.BadRequestException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final TokenProvider tokenProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+        String token = tokenProvider.resolveToken(httpServletRequest);
+
+        if (StringUtils.hasText(token) && tokenProvider.validateToken(token)) {
+            try {
+                Authentication authentication = tokenProvider.getAuthentication(token);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            } catch (BadRequestException ignore) {}
+        }
+
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
+    }
+}

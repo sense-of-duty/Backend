68f4cd8c35506ab4ee556919a1baf6c9ab2961ba
kimeoyeon <djdus126@gmail.com>
2025-11-26 00:09:37 +0900

    feat: FreeComment 도메인 생성 (#2)

--- GDG-Page/src/main/java/org/example/gdgpage/domain/freeboard/FreeComment.java
+++ GDG-Page/src/main/java/org/example/gdgpage/domain/freeboard/FreeComment.java
@@ -1,4 +1,55 @@
 package org.example.gdgpage.domain.freeboard;
 
-public class FreeComment {
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.gdgpage.domain.auth.User;
+import org.example.gdgpage.domain.common.BaseTimeEntity;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Table(name = "free_comments")
+public class FreeComment extends BaseTimeEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private FreePost post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id", nullable = false)
+    private User author;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private FreeComment parent;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    @Column(name = "is_anonymous", nullable = false)
+    private Boolean isAnonymous;
+
+    @Column(name = "like_count", nullable = false)
+    private Integer likeCount = 0;
+
+    public FreeComment(FreePost post, User author, String content, Boolean isAnonymous) {
+        this.post = post;
+        this.author = author;
+        this.content = content;
+        this.isAnonymous = isAnonymous;
+    }
+
+    public void update(String content) {
+        this.content = content;
+    }
+
+    public void increaseLikeCount() {
+        this.likeCount++;
+    }
 }

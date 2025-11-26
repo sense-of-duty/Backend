0c6cdebb42ecaada1d09ea1b274babaae10ae329
kimeoyeon <djdus126@gmail.com>
2025-11-27 03:42:58 +0900

    fix: 게시글 목록 조회 DTO 필드 추가 (#2)

--- GDG-Page/src/main/java/org/example/gdgpage/dto/freeboard/response/FreePostListResponseDto.java
+++ GDG-Page/src/main/java/org/example/gdgpage/dto/freeboard/response/FreePostListResponseDto.java
@@ -1,4 +1,45 @@
 package org.example.gdgpage.dto.freeboard.response;
 
+import lombok.Getter;
+import org.example.gdgpage.domain.freeboard.FreePost;
+
+import java.time.LocalDateTime;
+
+@Getter
 public class FreePostListResponseDto {
+
+    private Long id;
+    private String title;
+    private String previewContent;
+    private String authorName;
+    private Boolean isAnonymous;
+    private Boolean isPinned;
+    private Integer viewCount;
+    private Integer likeCount;
+    private Integer commentCount;
+    private LocalDateTime createdAt;
+
+    public FreePostListResponseDto(FreePost post) {
+
+        this.id = post.getId();
+        this.title = post.getTitle();
+        this.authorName = post.getIsAnonymous()
+                ? "익명"
+                : post.getAuthor().getName();
+
+        this.previewContent = createPreview(post.getContent());
+
+        this.isAnonymous = post.getIsAnonymous();
+        this.isPinned = post.getIsPinned();
+        this.viewCount = post.getViewCount();
+        this.likeCount = post.getLikeCount();
+        this.commentCount = post.getCommentCount();
+        this.createdAt = post.getCreatedAt();
+    }
+
+    private String createPreview(String content) {
+        return content.length() <= 70
+                ? content
+                : content.substring(0, 70) + "...";
+    }
 }

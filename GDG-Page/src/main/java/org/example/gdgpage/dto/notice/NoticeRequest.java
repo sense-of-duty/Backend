1fb1cdf5ee351424af69505248dfefba6a64f08e
Kim, Seovin <seovinkim832@gmail.com>
2025-11-28 21:13:46 +0900

    ãfeat:dto 생성(#3)

--- GDG-Page/src/main/java/org/example/gdgpage/dto/notice/NoticeRequest.java
+++ GDG-Page/src/main/java/org/example/gdgpage/dto/notice/NoticeRequest.java
@@ -1,4 +1,24 @@
 package org.example.gdgpage.dto.notice;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class NoticeRequest {
+
+
+    @NotBlank(message = "제목을 입력해주세요.")
+    private String title;
+
+
+    @NotBlank(message = "내용을 입력해주세요.")
+    private String content;
+
+
+    private boolean isPinned;
+
+//어느 파트 공지사항인지 보여주기
+    private Long partId;
 }
--- GDG-Page/src/main/java/org/example/gdgpage/dto/notice/NoticeResponse.java
+++ GDG-Page/src/main/java/org/example/gdgpage/dto/notice/NoticeResponse.java
@@ -1,4 +1,46 @@
 package org.example.gdgpage.dto.notice;
 
+import lombok.Builder;
+import lombok.Getter;
+import org.example.gdgpage.domain.notice.entity.Notice;
+
+import java.time.LocalDateTime;
+
+@Getter
 public class NoticeResponse {
+    private final Long id;
+    private final String title;
+    private final String content;
+    private final boolean isPinned;
+    private final int viewCount;
+    private final LocalDateTime createdAt;
+    private final Long authorId;
+    private final Long partId;
+
+
+    @Builder
+    public NoticeResponse(Long id, String title, String content, boolean isPinned, int viewCount, LocalDateTime createdAt, Long authorId, Long partId) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.isPinned = isPinned;
+        this.viewCount = viewCount;
+        this.createdAt = createdAt;
+        this.authorId = authorId;
+        this.partId = partId;
+    }
+
+
+    public static NoticeResponse from(Notice notice) {
+        return NoticeResponse.builder()
+                .id(notice.getId())
+                .title(notice.getTitle())
+                .content(notice.getContent())
+                .isPinned(notice.isPinned())
+                .viewCount(notice.getViewCount())
+                .createdAt(notice.getCreatedAt())
+                .authorId(notice.getAuthorId())
+                .partId(notice.getPartId())
+                .build();
+    }
 }

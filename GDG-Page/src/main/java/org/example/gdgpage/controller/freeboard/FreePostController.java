0565d8e4317221f47ef8193a6b5fcef62066740b
kimeoyeon <djdus126@gmail.com>
2025-12-14 15:59:04 +0900

    feat: FreePostController 추가 (#2)

---
+++ GDG-Page/src/main/java/org/example/gdgpage/controller/freeboard/FreePostController.java
@@ -0,0 +1,56 @@
+package org.example.gdgpage.controller.freeboard;
+
+import lombok.RequiredArgsConstructor;
+import org.example.gdgpage.domain.auth.User;
+import org.example.gdgpage.dto.freeboard.request.FreePostCreateRequestDto;
+import org.example.gdgpage.dto.freeboard.request.FreePostUpdateRequestDto;
+import org.example.gdgpage.dto.freeboard.response.FreePostListResponseDto;
+import org.example.gdgpage.dto.freeboard.response.FreePostResponseDto;
+import org.example.gdgpage.service.freeboard.FreePostService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/free-posts")
+public class FreePostController {
+
+    private final FreePostService freePostService;
+
+    @PostMapping
+    public FreePostResponseDto createPost(
+            @RequestBody FreePostCreateRequestDto dto,
+            @RequestAttribute User user
+    ) {
+        return freePostService.createPost(dto, user);
+    }
+
+    @PatchMapping("/{postId")
+    public FreePostResponseDto updatePost(
+            @PathVariable Long postId,
+            @RequestBody FreePostUpdateRequestDto dto,
+            @RequestAttribute User user
+    ) {
+        return freePostService.updatePost(postId, dto, user);
+    }
+
+    @GetMapping("/{postId}")
+    public FreePostResponseDto getPost(@PathVariable Long postId) {
+        return freePostService.getPost(postId);
+    }
+
+    @GetMapping
+    public List<FreePostListResponseDto> getPostList() {
+        return freePostService.getPostList();
+    }
+
+    @DeleteMapping("/{postId}")
+    public void deletePost(
+            @PathVariable Long postId,
+            @RequestAttribute User user
+    ) {
+        freePostService.deletePost(postId, user);
+    }
+
+}
